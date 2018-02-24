4cc4928a2f0f1e74a36dab2b031a8e30679c330c
dzianismakeichyk <dzianis.makeichyk@gmail.com>
2018-02-24 12:26:22 +0100

    add base sw events

---
+++ src/sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', (event) => {
+    // cache our bundle assets
+});
+
+self.addEventListener('activate', (event) => {
+    // remove old caches
+});
+
+self.addEventListener('fetch', (event) => {
+    
+});
