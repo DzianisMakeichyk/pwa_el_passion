58f0d31422e10120c83f62214b51a3dc0d4c1615
dzianismakeichyk <dzianis.makeichyk@gmail.com>
2018-02-24 12:51:48 +0100

    add base cache

--- src/sw.js
+++ src/sw.js
@@ -1,14 +1,11 @@
+const CACHE_NAME = 'vivino-cache-v3';
 const { assets } = global.serviceWorkerOption;
-
-console.log(assets)
 
 // cache our bundle assets
 self.addEventListener('install', (event) => {
     // Add all cache
-    let offlineRequest = new Request('../public/index.html');
-
     event.waitUntil(
-        fetch(offlineRequest).then(function(response) {
+        caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll(
                 assets
             );
@@ -22,17 +19,5 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-    let request = event.request;
 
-    event.respondWith(
-        fetch(request).catch(function(error)
-        {console.error(
-            '[onfetch] Failed. Serving cached offline fallback ' +
-            error
-        );
-            return caches.open('offline').then(function(cache) {
-                return cache.match('offline.html');
-            });
-        })
-    );
 });
