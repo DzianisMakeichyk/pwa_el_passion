cc25d0b86a0cf5d5e0994091a4297cd9ec6de9a7
dzianismakeichyk <dzianis.makeichyk@gmail.com>
2018-02-25 10:40:26 +0100

    add animation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,18 +2,19 @@ import React, { Fragment, Component } from 'react';
 import {
   BrowserRouter as Router,
   Route,
-  Switch,
 } from 'react-router-dom';
 import List from './List';
 import EmptyPage from './EmptyPage';
 import TabBar from './TabBar';
 import ItemShow from './ItemShow';
+import { AnimatedSwitch, withAnimatedWrapper } from './AnimatedSwitch';
+
 
 class App extends Component {
   state = {
     wines: [],
     isOffline: false,
-    areWeOnline: navigator.onLine
+    areWeOnline: navigator.onLine,
   };
 
   componentDidMount() {
@@ -23,7 +24,7 @@ class App extends Component {
 
       window.addEventListener('offline', this.upDateStatus);
 
-      this.upDateStatus()
+      this.upDateStatus();
   }
 
   upDateStatus = () => {
@@ -40,33 +41,35 @@ class App extends Component {
   };
 
   renderContent() {
+      const AnimatedEmptyPage = withAnimatedWrapper(EmptyPage);
+      const componentProps = { items: this.state.wines };
+
     if (!this.state.wines.length) {
       return <div />;
     }
 
     return (
       <Fragment>
-        <Switch>
-          <Route exact path="/" component={() => <List items={this.state.wines} />} />
-          <Route path="/wine/:id" component={() => <ItemShow items={this.state.wines} />} />
-          <Route path="/wishlist" component={EmptyPage} />
-          <Route path="/cellar" component={EmptyPage} />
-          <Route path="/articles" component={EmptyPage} />
-          <Route path="/profile" component={EmptyPage} />
-        </Switch>
+        <AnimatedSwitch>
+            <Route exact path="/" component={withAnimatedWrapper(List, componentProps)} />
+            <Route path="/wine/:id" component={withAnimatedWrapper(ItemShow, componentProps)} />
+            <Route path="/wishlist" component={AnimatedEmptyPage} />
+            <Route path="/cellar" component={AnimatedEmptyPage} />
+            <Route path="/articles" component={AnimatedEmptyPage} />
+            <Route path="/profile" component={AnimatedEmptyPage} />
+          </AnimatedSwitch>
         <TabBar />
       </Fragment>
     );
   }
 
   render() {
-    console.log(this.state)
     return (
       <Router>
         <Fragment>
           {this.state.isOffline && (
               <div>
-                Hello is online!
+                Hello offline!
               </div>
           )}
           {this.renderContent()}
